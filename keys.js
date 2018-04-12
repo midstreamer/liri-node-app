843245a367f97e10167e62e9ead9d6096041d634
midstreamer <chadalessi@yahoo.com>
2018-04-12 14:26:43 -0700

    added spotify key

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: 's8kCTglwHWVAEtjgbuCXV16ok',
+  consumer_secret: 'Ic5QnpJO8hXYcYpdIW2hcm2nO7tOk2O9BtWWOAMzwUqAR10jKM',
+  access_token_key: '984499355490058240-gg2UhJ1fRnA22KbgKKvW2NNQuA6MPDa',
+  access_token_secret: '7avRhPQhoUekF0b9dS0IWwL6WiYzUP0Df04R5BvliKa2I'
+};
+
+exports.spotify = {
+  id: 'b3fcb9894a52451f9892355a24be4c06',
+  secret: '59c372a673404e16a9f8645d81677425'
+};
